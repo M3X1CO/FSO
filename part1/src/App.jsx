d0142a9684c1cef4e5cd7dc7d57359903a5092aa
Howard <howard.felix@outlook.com>
2024-05-28 10:50:07 +0700

    part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,25 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26+10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App
