1ea75b6c61825e69865a837cf29b73f65c60a890
Howard <howard.felix@outlook.com>
2024-06-30 16:52:11 +0700

    new

--- part3/Notes/backend/index.js
+++ part3/Notes/backend/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 app.use(express.static('dist'));
 app.use(cors());
 app.use(express.json())
+app.use(requestLogger)
 
 app.use((request, response, next) => {
   console.log('--- Request Start ---')
@@ -49,7 +50,7 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -57,25 +58,48 @@ app.get('/api/notes/:id', (request, response) => {
       } else {
         response.status(404).end()
       }
-  }).catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  }).catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'Unknown Endpoint '})
 }
 
-app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-app.use((error, request, response, next) => {
-  console.error('Error:', error.message)
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'Malformatted ID '})
+    return response.status(400).send({ error: 'Malformated Id' })
   }
+
   next(error)
-})
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
