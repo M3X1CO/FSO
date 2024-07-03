84bbcc39ea6b0af62c98fb9bdbcac5fa32dbcc75
Howard <howard.felix@outlook.com>
2024-07-03 08:56:59 +0700

    uibuild

---
+++ part3/Phonebook/backend/eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+  },
+]
