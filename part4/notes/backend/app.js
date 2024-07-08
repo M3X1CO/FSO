e0b273c77796c093ff3caad7a8a9bd257a8dbb00
Howard <howard.felix@outlook.com>
2024-07-08 13:35:54 +0700

    uibuild

--- part4/notes/backend/app.js
+++ part4/notes/backend/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to db')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
