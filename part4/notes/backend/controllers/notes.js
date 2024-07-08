6992c8cbf56552c547cefd5dab4c8d05f4190024
Howard <howard.felix@outlook.com>
2024-07-08 14:10:43 +0700

    uibuild

--- part4/notes/backend/controllers/notes.js
+++ part4/notes/backend/controllers/notes.js
@@ -7,16 +7,12 @@ notesRouter.get('/', async (request, response) => {
 })
 
 notesRouter.get('/:id', async (request, response, next) => {
-  try {
     const note = await Note.findById(request.params.id)
     if (note) {
       response.json(note)
     } else {
       response.status(404).end()
     }
-  } catch (error) {
-    next(error)
-  }
 })
 
 notesRouter.post('/', async (request, response, next) => {
@@ -27,21 +23,13 @@ notesRouter.post('/', async (request, response, next) => {
     important: important || false,
   })
 
-  try {
     const savedNote = await note.save()
     response.status(201).json(savedNote)
-  } catch (error) {
-    next(error)
-  }
 })
 
 notesRouter.delete('/:id', async (request, response, next) => {
-  try {
     await Note.findByIdAndDelete(request.params.id)
     response.status(204).end()
-  } catch (error) {
-    next(error)
-  }
 })
 
 notesRouter.put('/:id', async (request, response, next) => {
@@ -52,12 +40,8 @@ notesRouter.put('/:id', async (request, response, next) => {
     important: important,
   }
 
-  try {
     const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true})
     response.json(updatedNote)
-  } catch (error) {
-    next(error)
-  }
 })
 
 module.exports = notesRouter
--- part4/notes/backend/tests/note_api.test.js
+++ part4/notes/backend/tests/note_api.test.js
@@ -9,11 +9,12 @@ const api = supertest(app)
 const Note = require('../models/note')
 
 beforeEach(async () => {
-    await Note.deleteMany({})
-    let noteObject = new Note(helper.initialNotes[0])
-    await noteObject.save()
-    noteObject = new Note(helper.initialNotes[1])
-    await noteObject.save()
+  await Note.deleteMany({})
+
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
 })
 
 test.only('notes are returned as json', async () => {
