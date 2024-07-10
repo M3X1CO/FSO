4f72205263acf076df151dfa5e971a073ecfcc8c
Howard <howard.felix@outlook.com>
2024-07-10 10:58:02 +0700

    uibuild

--- part4/notes/backend/controllers/notes.js
+++ part4/notes/backend/controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -16,14 +18,19 @@ notesRouter.get('/:id', async (request, response) => {
 })
 
 notesRouter.post('/', async (request, response) => {
-  const { content, important } = request.body
+  const { content, important, userId } = request.body
+
+  const user = await User.findById(userId)
 
   const note = new Note({
     content: content,
-    important: important || false,
+    important: important === undefined ? false : important,
+    user: user.id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
