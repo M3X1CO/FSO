4604748aec2cd03b8acf22e677034179358d6417
Howard <howard.felix@outlook.com>
2024-07-10 09:56:16 +0700

    uibuild

--- part4/notes/backend/controllers/notes.js
+++ part4/notes/backend/controllers/notes.js
@@ -3,45 +3,45 @@ const Note = require('../models/note')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
-    response.json(notes)
+  response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', async (request, response, next) => {
-  const {content, important} = request.body
+notesRouter.post('/', async (request, response) => {
+  const { content, important } = request.body
 
   const note = new Note({
     content: content,
     important: important || false,
   })
 
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-    await Note.findByIdAndDelete(request.params.id)
-    response.status(204).end()
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.put('/:id', async (request, response, next) => {
-  const {content, important} = request.body
+notesRouter.put('/:id', async (request, response) => {
+  const { content, important } = request.body
 
   const note = {
     content: content,
     important: important,
   }
 
-    const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true})
-    response.json(updatedNote)
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter
--- part4/notes/backend/utils/for_testing.js
+++ part4/notes/backend/utils/for_testing.js
@@ -1,19 +1,19 @@
 const reverse = (string) => {
-    return string
-      .split('')
-      .reverse()
-      .join('')
+  return string
+    .split('')
+    .reverse()
+    .join('')
+}
+
+const average = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item
   }
-  
-  const average = (array) => {
-    const reducer = (sum, item) => {
-      return sum + item
-    }
-  
-    return array.reduce(reducer, 0) / array.length
-  }
-  
-  module.exports = {
-    reverse,
-    average,
-  }
+
+  return array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
+  reverse,
+  average,
+}
