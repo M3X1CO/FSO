65383507949ad5401fe39077a7f8e43b4e35f864
Howard <howard.felix@outlook.com>
2024-07-13 10:45:42 +0700

    uibuild

--- part4/blogApp/frontend/src/services/blogs.js
+++ part4/blogApp/frontend/src/services/blogs.js
@@ -7,10 +7,12 @@ const setToken = newToken => {
   token = `Bearer ${newToken}`
 }
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  const response = await axios.get('/api/blogs', {
+    headers: { Authorization: token },
+  });
+  return response.data;
+};
 
 const create = async newObject => {
   const config = {
