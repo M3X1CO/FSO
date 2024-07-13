b8e3ede149ab3f3da1d3b251d61277cda3723e61
Howard <howard.felix@outlook.com>
2024-07-13 10:27:29 +0700

    uibuild

--- part4/blogApp/frontend/src/App.jsx
+++ part4/blogApp/frontend/src/App.jsx
@@ -160,29 +160,6 @@ const App = () => {
       </div>
       }
 
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-
       <h2>Blog List</h2>
       <ul>
         {blogs.map(blog => (
