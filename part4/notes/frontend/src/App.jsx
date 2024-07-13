647b7bba53028e5322275552238a51e25e66f64a
Howard <howard.felix@outlook.com>
2024-07-13 09:41:46 +0700

    uibuild

--- part4/notes/frontend/src/App.jsx
+++ part4/notes/frontend/src/App.jsx
@@ -22,6 +22,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
