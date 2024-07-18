bfce2e6d789d9bfc0a8d879fee1ab3e28808872f
Howard <howard.felix@outlook.com>
2024-07-18 12:45:22 +0700

    uibuild

--- part5/blogApp/backend/utils/middleware.js
+++ part5/blogApp/backend/utils/middleware.js
@@ -13,7 +13,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  console.log('error:', error)
+  logger.error('error:', error)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
