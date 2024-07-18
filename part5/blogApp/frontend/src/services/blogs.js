ff602cfa4cff1c3776a48d9163879cd888b1e1d1
Howard <howard.felix@outlook.com>
2024-07-18 12:16:56 +0700

    uibuild

--- part5/blogApp/frontend/src/services/blogs.js
+++ part5/blogApp/frontend/src/services/blogs.js
@@ -16,7 +16,7 @@ const create = async newObject => {
   const config = {
     headers: { Authorization: token },
   }
-  
+
   const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
