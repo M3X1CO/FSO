451dad2a72cc790391fab20589a3fd04ef308821
Howard <howard.felix@outlook.com>
2024-07-24 12:17:08 +0700

    uibuild

--- part5/blogApp/frontend/src/components/Blog.jsx
+++ part5/blogApp/frontend/src/components/Blog.jsx
@@ -1,62 +1,71 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
+const BlogDetails = ({ blog, handleLike, handleDelete, currentUser }) => (
+  <div className="blog-details">
+    <p>URL: {blog.url}</p>
+    <p>Votes: {blog.votes} <button data-testid='like-button' onClick={() => handleLike(blog.id)}>Like</button></p>
+    {blog.user && <p>Added by {blog.user.name}</p>}
+    {currentUser && blog.user && currentUser.username === blog.user.username && (
+      <button
+        data-testid='delete-button'
+        onClick={() => handleDelete(blog.id)}
+        className="delete-button">
+        Delete
+      </button>
+    )}
+  </div>
+)
+
 const Blog = ({ blog, handleLike, handleDelete, currentUser }) => {
   const [showDetails, setShowDetails] = useState(false)
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails)
-  }
+  const toggleDetails = () => setShowDetails(!showDetails)
 
   const confirmDelete = () => {
     if (window.confirm(`Are you sure you want to delete ${blog.title}?`)) {
-      handleDelete(blog.id) 
+      handleDelete(blog.id)
     }
   }
 
-  console.log('Current User:', currentUser)
-  console.log('Blog User:', blog.user)
-
   return (
-    <li className='blog'>
+    <article className='blog'>
       <div>
         <strong>Title: {blog.title}</strong> Author: {blog.author}
-        <button
-          data-testid='toggle-details'
-          onClick={toggleDetails}>
+        <button data-testid='toggle-details' onClick={toggleDetails}>
           {showDetails ? 'Hide' : 'View'}
         </button>
       </div>
       {showDetails && (
-        <div>
-          <p>{blog.url}</p>
-          <p>{blog.votes} Votes</p>
-          <button
-            data-testid='like-button'
-            onClick={() => handleLike(blog.id)}
-            >Like
-          </button>
-          {blog.user && <p>Added by {blog.user.name}</p>}
-          {currentUser && blog.user && currentUser.username === blog.user.username && (
-            <button
-              data-testid='delete-button'
-              data-user-id={blog.user}
-              onClick={confirmDelete}
-              className="delete-button">
-              Delete
-            </button>
-          )}
-        </div>
+        <BlogDetails 
+          blog={blog} 
+          handleLike={handleLike} 
+          handleDelete={confirmDelete} 
+          currentUser={currentUser}
+        />
       )}
-    </li>
+    </article>
   )
 }
 
 Blog.propTypes = {
-  blog: PropTypes.object.isRequired,
+  blog: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    votes: PropTypes.number.isRequired,
+    user: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  }).isRequired,
   handleLike: PropTypes.func.isRequired,
   handleDelete: PropTypes.func.isRequired,
-  currentUser: PropTypes.object
+  currentUser: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  })
 }
 
 export default Blog
