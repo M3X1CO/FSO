317fe9c9c0389c02404ce8f338e4a058eecaef1a
Howard <howard.felix@outlook.com>
2024-07-31 11:46:33 +0700

    update

--- part6/blogApp/backend/README.md
+++
@@ -1,4 +0,0 @@
-# FSO-Backend
-Test environment
-
-Double
--- part6/notes/backend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app') // The Express app
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
